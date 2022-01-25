e8d34af8b754c0c884eefd2a19f345fae23aa579
FAii43 <636051200302@mail.rmutk.ac.th>
2022-01-25 12:11:24 +0700

    Initial commit

---
+++ app/src/main/java/com/example/a5/MainActivity.kt
@@ -0,0 +1,105 @@
+package com.example.a5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bc.setOnClickListener {
+            tv.setText("0")
+        }
+        b7.setOnClickListener {
+            if (tv.text == "0") {
+                tv.setText("")
+                tv.setText(tv.text.toString() + "7")
+            } else {
+                tv.setText(tv.text.toString() + "7")
+            }
+        }
+        b8.setOnClickListener {
+            if (tv.text == "0") {
+                tv.setText("")
+                tv.setText(tv.text.toString() + "8")
+            } else {
+                tv.setText(tv.text.toString() + "8")
+            }
+        }
+        b9.setOnClickListener {
+            if (tv.text == "0") {
+                tv.setText("")
+                tv.setText(tv.text.toString() + "9")
+            } else {
+                tv.setText(tv.text.toString() + "9")
+            }
+        }
+        b4.setOnClickListener {
+            if (tv.text == "0") {
+                tv.setText("")
+                tv.setText(tv.text.toString() + "4")
+            } else {
+                tv.setText(tv.text.toString() + "4")
+            }
+        }
+        b5.setOnClickListener {
+            if (tv.text == "0") {
+                tv.setText("")
+                tv.setText(tv.text.toString() + "5")
+            } else {
+                tv.setText(tv.text.toString() + "5")
+            }
+        }
+        b6.setOnClickListener {
+            if (tv.text == "0") {
+                tv.setText("")
+                tv.setText(tv.text.toString() + "6")
+            } else {
+                tv.setText(tv.text.toString() + "6")
+            }
+        }
+        b1.setOnClickListener {
+            if (tv.text == "0") {
+                tv.setText("")
+                tv.setText(tv.text.toString() + "1")
+            } else {
+                tv.setText(tv.text.toString() + "1")
+            }
+        }
+        b2.setOnClickListener {
+            if (tv.text == "0") {
+                tv.setText("")
+                tv.setText(tv.text.toString() + "2")
+            } else {
+                tv.setText(tv.text.toString() + "2")
+            }
+        }
+        b3.setOnClickListener {
+            if (tv.text == "0") {
+                tv.setText("")
+                tv.setText(tv.text.toString() + "3")
+            } else {
+                tv.setText(tv.text.toString() + "3")
+            }
+        }
+        b0.setOnClickListener {
+            if (tv.text == "0") {
+                tv.setText("0")
+            } else {
+                tv.setText(tv.text.toString() + "0")
+            }
+        }
+
+        bd.setOnClickListener {
+            if (tv.text == "0" || tv.text.length.toString() == "1") {
+                tv.setText("0")
+            } else {
+                tv.setText(tv.text.toString().dropLast(1))
+            }
+        }
+
+    }
+}
